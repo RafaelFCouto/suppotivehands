aa6a1991d9e32a0ec1c32e56e77a430f393e8a6b
RafaelFCouto <rafaelfreirecouto@outlook.com>
2025-01-07 20:13:05 -0400

    remove unused models

--- models/index.js
+++ models/index.js
@@ -1,22 +1,13 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const User = require('./user');
-const Institution = require('./institution');
-const Need = require('./need');
-const DonationType = require('./donation-type');
 
 User.init(sequelize);
-Institution.init(sequelize);
-Need.init(sequelize);
-DonationType.init(sequelize);
 
 const models = {
   sequelize,
   Sequelize,
-  User,
-  Institution,
-  Need,
-  DonationType
+  User
 };
 
 module.exports = models;
